b07b8d4dcf2b15ca983176ab30fd8a262903542a
Hajime Nagasaka <hajimenagasaka@HajimenoMBP.lan>
2018-07-08 19:00:31 +0900

    initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:"OQdTo8ZtIXUTe6jGw2NZcgW2u",
+  consumer_secret:"8b8sjjdk0MrzdPdL2ER2v4cTkZM9Neh32RsyXLNOfY24ZxMGIQ",
+
+  access_token_key:"766044170247102466-W9Vqymi25bsb5Z4kMq2yUD1hpGD4mdh",
+  access_token_secret:"ouQGgWbBJyMT2rPLBvDiq1LIzThlzp0ixzqs0qU0CUSph"
+}
